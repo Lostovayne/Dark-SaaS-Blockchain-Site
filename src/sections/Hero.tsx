0e60738e6a6c61e6f67297f3f8c91f11a24e10e5
LostoVayne <moises.barrera.dev@gmail.com>
2025-06-21 01:16:45 -0400

    fix: update package.json dependencies and improve code formatting in HeroSection

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -12,10 +12,7 @@ export const HeroSection = () => {
   const cuboidRef = useRef<HTMLImageElement | null>(null);
 
   // Takes the scroll progress and uses it to animate the hexagon
-  const { scrollYProgress } = useScroll({
-    target: icosahedronRef,
-    offset: ["start end", "end start"],
-  });
+  const { scrollYProgress } = useScroll({ target: icosahedronRef, offset: ["start end", "end start"] });
 
   const { scrollYProgress: cubeScrollYProgress } = useScroll({
     target: cubeRef,
@@ -40,15 +37,12 @@ export const HeroSection = () => {
   return (
     <section className="py-24 md:py-52 overflow-x-clip">
       <div className="container mx-auto">
-        <p className="uppercase font-extrabold text-center text-zinc-500 tracking-wider">
-          Introducing Blockforge
-        </p>
+        <p className="uppercase font-extrabold text-center text-zinc-500 tracking-wider">Introducing Blockforge</p>
         <h1 className="font-heading font-black text-5xl md:text-6xl lg:text-7xl text-center mt-4 max-w-3xl mx-auto">
           The Future of Blockchain is Here.
         </h1>
         <p className="text-center text-xl md:text-2xl mt-6 text-zinc-400 max-w-xl mx-auto ">
-          Blockforge is pioneering smart contract integrity with cutting-edge
-          data solutions.
+          Blockforge is pioneering smart contract integrity with cutting-edge data solutions.
         </p>
         <div className="flex justify-center mt-10">
           <CutCornerButton className="">Get Started</CutCornerButton>
@@ -89,9 +83,7 @@ export const HeroSection = () => {
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <Circle className="absolute -left-[600px] -top-[80px] ">
                 <motion.img
-                  style={{
-                    rotate: TorusRotate,
-                  }}
+                  style={{ rotate: TorusRotate }}
                   ref={torusRef}
                   src="/assets/images/torus.png"
                   alt="Torus 3d"
@@ -100,35 +92,23 @@ export const HeroSection = () => {
               </Circle>
             </div>
 
-            <motion.div
-              style={{
-                rotate: IcosanhedronRotate,
-              }}
-              className={"inline-flex"}
-              ref={icosahedronRef}
-            >
+            <motion.div style={{ rotate: IcosanhedronRotate }} className={"inline-flex"} ref={icosahedronRef}>
               <img
                 src="/assets/images/icosahedron.png"
                 className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 saturate[10%] brightness-4 hue-rotate-240"
                 alt=""
               />
 
-              <img
-                src="/assets/images/icosahedron.png"
-                alt="Icosanhedron 3D"
-                className="w-[500px]"
-              />
+              <img src="/assets/images/icosahedron.png" alt="Icosanhedron 3D" className="w-[500px]" />
             </motion.div>
           </div>
         </div>
 
         <div className="flex flex-col justify-center items-center mt-40 md:mt-80  gap-4">
-          <div className=" h-10 w-5 outline outline-[6px] outline-fuchsia-500/10 inline-flex justify-center pt-2 rounded-full">
+          <div className=" h-10 w-5  outline-[6px] outline-fuchsia-500/10 inline-flex justify-center pt-2 rounded-full">
             <div className="h-3 w-1 bg-fuchsia-500 rounded-full"></div>
           </div>
-          <p className="uppercase text-zinc-500 font-extrabold tracking-wider">
-            Scroll to learn more
-          </p>
+          <p className="uppercase text-zinc-500 font-extrabold tracking-wider">Scroll to learn more</p>
         </div>
       </div>
     </section>
