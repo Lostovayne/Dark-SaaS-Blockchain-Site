86b9d0df1e05a59c1315c4b8d0a3f34d9bf736b1
LostoVayne <moises.barrera.dev@gmail.com>
2025-08-10 19:27:22 -0400

    fix astro version

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,34 +1,36 @@
+import { motion } from "framer-motion";
 import { type ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
-import { motion } from "framer-motion";
 
 export const Circle = ({
-	children,
-	className,
-	animate = false
+  children,
+  className,
+  animate = false,
 }: ComponentPropsWithoutRef<"div"> & { animate?: boolean }) => {
-	return (
-		<div
-			className={twMerge(
-				"bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full relative",
-				className
-			)}>
-			<motion.div
-				animate={
-					animate && {
-						rotate: 360
-					}
-				}
-				transition={{
-					ease: "linear",
-					duration: 15,
-					repeat: Infinity
-				}}
-				className={twMerge(
-					"absolute inset-0 rounded-full outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10 border-[6px] border-transparent ",
-					animate && "border-t-fuchsia-500/30"
-				)}></motion.div>
-			{children}
-		</div>
-	);
+  return (
+    <div
+      className={twMerge(
+        "bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full relative",
+        className
+      )}
+    >
+      <motion.div
+        animate={
+          animate && {
+            rotate: 360,
+          }
+        }
+        transition={{
+          ease: "linear",
+          duration: 15,
+          repeat: Infinity,
+        }}
+        className={twMerge(
+          "absolute inset-0 rounded-full  outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10 border-[6px] border-transparent ",
+          animate && "border-t-fuchsia-500/30"
+        )}
+      ></motion.div>
+      {children}
+    </div>
+  );
 };
--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -1,79 +1,105 @@
-import Card from '@/components/Card';
+import Card from "@/components/Card";
+import { div } from "framer-motion/client";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const cardData = [
-	{
-		image: '/assets/images/pill.png',
-		title: 'Revolutionary Blockchain API',
-		description:
-			'Effortlessly integrate and manage blockchain data with our cutting-edge API, designed for seamless connectivity',
-		color: 'fuchsia',
-	},
-	{
-		image: '/assets/images/cuboid.png',
-		title: 'Decentralized Data Solutions',
-		description:
-			'Empower your applucations with decentralized data solutions, ensuring security and transparency at every step',
-		color: 'lime',
-	},
-	{
-		image: '/assets/images/cone.png',
-		title: 'Next-Gen Smart Contracts',
-		description:
-			'Unlock the potential of next-gen smart contracts with our robust and scalable API, tailored for modern blockchain needs',
-		color: 'cyan',
-	},
-	{
-		image: '/assets/images/icosahedron.png',
-		title: 'Seamless Bloockchain Integration',
-		description:
-			'Integrate blockchain technology seamlessly into your projects, with minimal effort and maximum efficiency',
-		color: 'violet',
-	},
+  {
+    image: "/assets/images/pill.png",
+    title: "Revolutionary Blockchain API",
+    description:
+      "Effortlessly integrate and manage blockchain data with our cutting-edge API, designed for seamless connectivity",
+    color: "fuchsia",
+  },
+  {
+    image: "/assets/images/cuboid.png",
+    title: "Decentralized Data Solutions",
+    description:
+      "Empower your applucations with decentralized data solutions, ensuring security and transparency at every step",
+    color: "lime",
+  },
+  {
+    image: "/assets/images/cone.png",
+    title: "Next-Gen Smart Contracts",
+    description:
+      "Unlock the potential of next-gen smart contracts with our robust and scalable API, tailored for modern blockchain needs",
+    color: "cyan",
+  },
+  {
+    image: "/assets/images/icosahedron.png",
+    title: "Seamless Bloockchain Integration",
+    description:
+      "Integrate blockchain technology seamlessly into your projects, with minimal effort and maximum efficiency",
+    color: "violet",
+  },
 ];
 
 const FeaturesCardsSection = () => {
-	return (
-		<section className='py-24 overflow-clip md:-mt-28'>
-			<div className='container mx-auto'>
-				<h2 className='font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center'>
-					Discover the future of blockchain with Blockforge.
-				</h2>
-				<div className='mt-36 lg:mt-48 flex'>
-					<div className='flex flex-none gap-8'>
-						{cardData.map(({ image, title, description, color }) => (
-							<Card
-								key={title}
-								className='max-w-xs md:max-w-md'
-								pdbutton>
-								<div className='flex justify-center -mt-28'>
-									<div className='inline-flex relative'>
-										<div className='absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] mask-[radial-gradient(closest-side,black,transparent)]'></div>
-										<img
-											src={image}
-											alt='Pill'
-											className='size-40 group-hover:-translate-y-6 transition duration-300'
-										/>
-									</div>
-								</div>
-								<h3 className='font-heading font-black text-3xl mt-12'>{title}</h3>
-								<p className='text-lg text-zinc-400 mt-4'>{description}</p>
-							</Card>
-						))}
-					</div>
-				</div>
+  const [selectedCardIndex, setSelectedCardIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
 
-				<div className='flex justify-center mt-10'>
-					<div className='bg-zinc-950 inline-flex gap-4 p-2.5 rounded-full'>
-						{cardData.map(({ title }) => (
-							<div
-								key={title}
-								className='size-2.5 bg-zinc-500 rounded-full cursor-pointer'></div>
-						))}
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  useEffect(() => {
+    if (isHovered) return;
+    const timeout = setTimeout(() => {
+      setSelectedCardIndex((selectedCardIndex + 1) % cardData.length);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [selectedCardIndex, isHovered]);
+
+  return (
+    <section className="py-24 overflow-clip md:-mt-28">
+      <div className="container mx-auto">
+        <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center">
+          Discover the future of blockchain with Blockforge.
+        </h2>
+        <div className="mt-36 lg:mt-48 flex">
+          <div className="flex flex-none gap-8">
+            {cardData.map(({ image, title, description, color }, cardIndex) => (
+              <div
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                key={cardIndex}
+                className="inline-flex transition-all duration-500"
+                style={{
+                  transform: `translateX(calc((-100% - 2rem) * ${selectedCardIndex}))`,
+                }}
+              >
+                <Card key={title} className="max-w-xs md:max-w-md" pdbutton>
+                  <div className="flex justify-center -mt-28">
+                    <div className="inline-flex relative">
+                      <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] mask-[radial-gradient(closest-side,black,transparent)]"></div>
+                      <img
+                        src={image}
+                        alt="Pill"
+                        className="size-40 group-hover:-translate-y-6 transition duration-300"
+                      />
+                    </div>
+                  </div>
+                  <h3 className="font-heading font-black text-3xl mt-12">{title}</h3>
+                  <p className="text-lg text-zinc-400 mt-4">{description}</p>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex justify-center mt-10">
+          <div className="bg-zinc-950 inline-flex gap-4 p-2.5 rounded-full">
+            {cardData.map(({ title }, cardIndex) => (
+              <div
+                key={title}
+                className={twMerge(
+                  "size-2.5 bg-zinc-500 rounded-full cursor-pointer",
+                  cardIndex === selectedCardIndex && "bg-zinc-300"
+                )}
+                onClick={() => setSelectedCardIndex(cardIndex)}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FeaturesCardsSection;
